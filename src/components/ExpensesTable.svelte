13e5ab04a3bc51435b2d18ec9b074a64ffff353b
Yishai Zehavi <zehaviyishai@gmail.com>
2020-09-30 16:10:10 +0300

    implemented "edit expense" functionality

--- src/components/ExpensesTable.svelte
+++ src/components/ExpensesTable.svelte
@@ -1,11 +1,14 @@
 <script>
-  import { getStoreForCategory } from '../stores/expenses';
+import { getContext } from 'svelte';
+import { getStoreForCategory } from '../stores/expenses';
+import EditExpense from './modals/EditExpense.svelte';
 
+  const { open, close } = getContext('simple-modal');
 
   // Duplicate from `stores/expenses.js`!
   /**
    * @typedef {String} ID
-   * @typedef {{id?: ID, item: string, cost: number}} Expense
+   * @typedef {{id?: ID, item: string, cost: number, date: number, notes: string}} Expense
    * @typedef {import('svelte/store').Readable<Expense[]> & {
    *  addExpense: (expense: Expense) => ID,
    *  updateExpense: (id: ID, expense: Expense) => ID,
@@ -16,49 +19,64 @@
   export let categoryId;
   $: store = getStoreForCategory(categoryId);
   $: expenses = $store;
-  $: console.log(expenses);
+
+  const updateExpense = (expense) => () => {
+    open(EditExpense, {
+      expense,
+      updateEntry: (updatedExpense) => {
+        store.updateExpense(updatedExpense.id, updatedExpense);
+        close();
+      }
+    });
+  }
 </script>
 
-<table class="min-w-full leading-normal">
+<table class="{$$props.class} min-w-full leading-normal">
   <thead>
-      <tr>
+      <tr class="flex">
           <th
-              class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              class="flex-1 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
               פריט
           </th>
           <th
-              class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              class="flex-1 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
               עלות
           </th>
           <th
-              class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              class="flex-1 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
               תאריך
           </th>
           <th
-              class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              class="flex-1 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
               הערות
           </th>
       </tr>
   </thead>
   <tbody class="text-center">
       {#each expenses as expense}
-      <tr>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+      <tr class="flex cursor-pointer bg-white text-gray-900 hover:bg-purple-300 hover:text-white" on:click={updateExpense(expense)}>
+        <td class="flex-1 px-5 py-5 border-b border-gray-200 text-sm">
+          <p>
               {expense.item}
           </p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td class="flex-1 px-5 py-5 border-b border-gray-200 text-sm">
+          <p>
               {expense.cost} ₪
           </p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td class="flex-1 px-5 py-5 border-b border-gray-200 text-sm">
+          <p>
+              {
+                new Date(expense.date).toLocaleDateString('he-IL', {
+                  dateStyle: 'short',
+                })
+              }
           </p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td class="flex-1 px-5 py-5 border-b border-gray-200 text-sm">
+          <p>
+              {expense.notes}
           </p>
         </td>
     </tr>
---
+++ src/components/modals/EditExpense.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let expense;
+  export let updateEntry = () => {};
+
+  let item = expense.item;
+  let cost = expense.cost;
+  let notes = expense.notes;
+</script>
+
+<div class="flex flex-col mr-8">
+  <h1 class="text-lg mb-3">עריכה</h1>
+
+  <div class="grid grid-flow-row gap-y-3 mb-3">
+    <label class="flex flex-col">
+      <span class="text-sm font-bold">שם הפריט</span>
+      <input
+        type="text"
+        class="px-3 py-2 bg-gray-200 focus:bg-gray-300 rounded-lg text-gray-900"
+        dir="rtl"
+        bind:value={item} />
+    </label>
+    <label class="flex flex-col">
+      <span class="text-sm font-bold">עלות הפריט</span>
+      <input
+        type="number"
+        class="px-3 py-2 bg-gray-200 focus:bg-gray-300 rounded-lg text-gray-900"
+        dir="rtl"
+        bind:value={cost} />
+    </label>
+    <label class="flex flex-col">
+      <span class="text-sm font-bold">הערות</span>
+      <textarea
+        class="px-3 py-2 bg-gray-200 focus:bg-gray-300 rounded-lg text-gray-900"
+        dir="rtl"
+        bind:value={notes}></textarea>
+    </label>
+  </div>
+  <button
+    class="bg-blue-300 text-gray-800 rounded-lg px-3 py-2 self-end"
+    on:click={() => updateEntry({ id: expense.id, item, cost, date: expense.date, notes })}>עדכן שורה בטבלה</button>
+</div>
