12a628f3fab1f71794a68e378b334e3593af2028
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-01 12:32:42 +0300

    rename Nav to SideNav

---
+++ src/components/SideNav.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { categories } from '../stores/categories';
+import Pen from './svg/Pen.svelte';
+
+  export let segment = 'home';
+</script>
+
+<style lang="postcss">
+  .active {
+    @apply border-r-4 border-purple-400 text-purple-400 font-medium;
+  }
+</style>
+
+<nav class="{$$props.class} flex flex-col items-stretch px-3 overflow-hidden">
+  <a rel="prefetch" href="/categories" class="self-start flex items-center gap-x-1 mb-2">
+    <Pen class="h-3 w-3" />
+    <span class="text-xs tracking-wide text-gray-700 hover:text-black underline">עריכת קטגוריות</span>
+  </a>
+  <ul class="flex-1 mb-2 overflow-y-auto">
+    {#each $categories as category}
+      <li class="font-light" class:active="{segment === category.id}">
+        <a rel="prefetch" href="view/{category.id}"
+          class="flex items-center gap-x-2 p-2 hover:text-purple-400">
+          <span class="text-sm tracking-wide">{category.name}</span>
+        </a>
+      </li>
+    {/each}
+  </ul>
+</nav>
--- src/routes/view/_layout.svelte
+++ src/routes/view/_layout.svelte
@@ -1,11 +1,11 @@
 <script>
-  import Nav from '../../components/Nav.svelte';
+  import SideNav from '../../components/SideNav.svelte';
 
   export let segment;
 </script>
 
-<div class="h-screen w-full flex flex-row overflow-hidden">
-  <Nav {segment} class="w-1/4 h-full py-4" />
+<div class="flex-1 flex flex-row overflow-hidden">
+  <SideNav {segment} class="w-1/4 h-full py-4" />
 
   <slot />
 </div>
