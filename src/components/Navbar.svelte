7b84e3f9068259cead60ff2386029194d827b7fb
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-05 21:55:59 +0300

    added current month to Navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,22 @@
 <script>
   export let segment;
+
+  const months = [
+    'ינואר',
+    'פברואר',
+    'מרץ',
+    'אפריל',
+    'מאי',
+    'יוני',
+    'יולי',
+    'אוגוסט',
+    'ספטמבר',
+    'אוקטובר',
+    'נובמבר',
+    'דצמבר'
+  ];
+  const hebMonthName = months[new Date().getMonth()];
+  const year = new Date().getFullYear();
 </script>
 
 <style lang="postcss">
@@ -8,7 +25,7 @@
   }
 </style>
 
-<nav class="border-b border-gray-300">
+<nav class="flex justify-between border-b border-gray-300">
   <ul class="flex items-center gap-x-3 px-3">
     <li class="px-2 py-1 font-light tracking-wide" class:active="{segment === undefined}">
       <a rel="prefetch" href="/" class="text-sm text-gray-700">עמוד הבית</a>
@@ -20,4 +37,7 @@
       <a rel="prefetch" href="/statistics" class="text-sm text-gray-700">סטטיסטיקות</a>
     </li>
   </ul>
+  <div class="flex items-center px-3">
+    <span class="font-bold tracking-wide">{hebMonthName} {year}</span>
+  </div>
 </nav>
