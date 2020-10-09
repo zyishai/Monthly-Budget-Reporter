9d96bfe82c960b2d2084bd02df2957cc5fe73cd0
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-09 15:56:39 +0300

    adjusted navbar for mobile

---
+++ src/components/HamburgerMenu.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let open = false;
+
+  const openMenu = () => open = true;
+  const closeMenu = () => open = false;
+  const toggleMenu = () => open = !open;
+</script>
+
+<style lang="postcss">
+  #hamburger {
+    transition: .3s;
+  }
+
+  #hamburger div {
+    position: absolute;
+    top: calc(50% - 1px);
+    height: 2px;
+    width: 100%;
+    background-color: #000;
+  }
+
+  #hamburger::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 70%;
+    height: 2px;
+    background-color: #000;
+    transform-origin: right;
+    transition: .3s;
+  }
+  #hamburger::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 40%;
+    height: 2px;
+    background-color: #000;
+    transform-origin: right;
+    transition: .3s;
+  }
+
+  #hamburger.open {
+    transform: rotate(45deg);
+  }
+  #hamburger.open::before {
+    top: calc(-50% + 1px);
+    transform: rotate(-90deg); 
+    width: 50%;
+  }
+  #hamburger.open::after {
+    bottom: calc(50% - 1px);
+    transform: rotate(-90deg);
+    width: 50%;
+  }
+</style>
+
+<div id="hamburger" class="sm:hidden relative mr-auto w-5 h-3 cursor-pointer" class:open on:click={toggleMenu}>
+  <div></div>
+</div>
+
+{#if open}
+  <slot {openMenu} {closeMenu} {toggleMenu}></slot>
+{/if}
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,22 @@
 <script>
+  import { slide } from 'svelte/transition';
+  import HamburgerMenu from './HamburgerMenu.svelte';
   export let segment;
+
+  const pages = [
+    {
+      title: 'עמוד הבית',
+      url: '',
+    },
+    {
+      title: 'קטגוריות',
+      url: 'view',
+    },
+    {
+      title: 'סטטיסטיקות',
+      url: 'statistics',
+    },
+  ];
 
   const months = [
     'ינואר',
@@ -23,21 +40,48 @@
   .active {
     @apply border-b-2 border-purple-400 font-medium;
   }
+
+  @media screen and (max-width: 640px) {
+    nav > #mobile-nav {
+      position: absolute;
+      top: calc(100% + 2px);
+      height: calc(100vh - 100% - 2px);
+      width: 100%;
+      background-color: #fff;
+      z-index: 1000;
+    }
+    
+    .active {
+      @apply border-b-0 font-semibold;
+    }
+  }
+
 </style>
 
-<nav class="flex justify-between border-b border-gray-300">
-  <ul class="flex items-center gap-x-3 px-3">
-    <li class="px-2 py-1 font-light tracking-wide" class:active="{segment === undefined}">
-      <a rel="prefetch" href="/" class="text-sm text-gray-700">עמוד הבית</a>
-    </li>
-    <li class="px-2 py-1 font-light tracking-wide" class:active="{segment === 'view' || segment === 'categories'}">
-      <a rel="prefetch" href="/view" class="text-sm text-gray-700">קטגוריות</a>
-    </li>
-    <li class="px-2 py-1 font-light tracking-wide" class:active="{segment === 'statistics'}">
-      <a rel="prefetch" href="/statistics" class="text-sm text-gray-700">סטטיסטיקות</a>
-    </li>
+<nav class="relative flex items-center px-3 py-2 sm:py-0 mb-3 sm:mb-0 border-b border-gray-300">
+  <ul class="hidden sm:flex items-center gap-x-3">
+    {#each pages as page}
+      <li class="px-2 py-1 font-light tracking-wide" class:active={segment === page.url || (!page.url && segment === undefined)}>
+        <a rel="prefetch" href="/{page.url}" class="text-sm text-gray-700">{page.title}</a>
+      </li>
+    {/each}
   </ul>
-  <div class="flex items-center px-3">
+  <div class="flex items-center mr-auto">
     <span class="font-bold tracking-wide">{hebMonthName} {year}</span>
   </div>
+  <HamburgerMenu 
+    let:closeMenu>
+    <ul 
+      id="mobile-nav" 
+      class="flex sm:hidden flex-col justify-center items-center gap-y-3 -mx-3 bg-gray-200"
+      transition:slide={{
+        duration: 300
+      }}>
+      {#each pages as page}
+        <li class="sm:px-2 sm:py-1 font-normal sm:font-light tracking-widest sm:tracking-wide" class:active={segment === page.url || (!page.url && segment === undefined)}>
+          <a rel="prefetch" href="/{page.url}" class="text-xl sm:text-sm text-gray-700" on:click={closeMenu}>{page.title}</a>
+        </li>
+      {/each}
+    </ul>
+  </HamburgerMenu>
 </nav>
