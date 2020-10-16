7bffa99f1d46c6f0cff76781fad6d1f680b30596
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-16 15:07:47 +0300

    update modals to require important fields

--- src/components/modals/AddCategory.svelte
+++ src/components/modals/AddCategory.svelte
@@ -12,35 +12,44 @@
   });
 
   let categoryName = '';
-  let maxExpense;
+  let maxExpense = 0;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addCategory({
+      name: categoryName,
+      maxExpense,
+    });
+  }
 </script>
 
 <div class="flex flex-col mr-8">
   <h1 class="text-lg mb-3">הוספת קטגוריה</h1>
 
-  <div class="grid grid-flow-row gap-y-3 mb-3">
-    <label class="flex flex-col">
-      <span class="text-sm font-medium">שם הקטגוריה</span>
-      <input
-        type="text"
-        class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
-        dir="rtl"
-        bind:value={categoryName}
-        bind:this="{categoryNameInput}" />
-    </label>
-    <label class="flex flex-col">
-      <span class="text-sm font-medium">הוצאה מקסימלית</span>
-      <input
-        type="text"
-        class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
-        dir="rtl"
-        bind:value={maxExpense} />
-    </label>
-  </div>
-  <button
-    class="tracking-wide px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded self-end"
-    on:click={() => addCategory({
-        name: categoryName,
-        maxExpense,
-      })}>שמירת קטגוריה</button>
+  <form on:submit={onSubmit}>
+    <div class="grid grid-flow-row gap-y-3 mb-3">
+      <label class="flex flex-col">
+        <span class="text-sm font-medium">שם הקטגוריה</span>
+        <input
+          type="text"
+          class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
+          dir="rtl"
+          bind:value={categoryName}
+          bind:this="{categoryNameInput}"
+          required={true} />
+      </label>
+      <label class="flex flex-col">
+        <span class="text-sm font-medium">הוצאה מקסימלית</span>
+        <input
+          type="text"
+          class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
+          dir="rtl"
+          bind:value={maxExpense}
+          required={true} />
+      </label>
+    </div>
+    <button
+      class="tracking-wide px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded self-end"
+      type="submit">שמירת קטגוריה</button>
+  </form>
 </div>
--- src/components/modals/AddExpense.svelte
+++ src/components/modals/AddExpense.svelte
@@ -13,49 +13,58 @@ import { onMount } from "svelte";
   });
 
   let item = '';
-  let cost;
+  let cost = 0;
   let notes = '';
   let date = new Date().toLocaleDateString('en-CA'); // also: new Date().toISOString().slice(0, 10)
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addRow({ item, cost, notes, date });
+  }
 </script>
 
 <div class="flex flex-col mr-8">
   <h1 class="text-lg mb-3">הוספת פריט</h1>
 
-  <div class="grid grid-flow-row gap-y-3 mb-3">
-    <label class="flex flex-col">
-      <span class="text-sm font-medium">שם הפריט</span>
-      <input
-        type="text"
-        class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
-        dir="rtl"
-        bind:value={item}
-        bind:this={itemNameInput} />
-    </label>
-    <label class="flex flex-col">
-      <span class="text-sm font-medium">עלות הפריט</span>
-      <input
-        type="number"
-        class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
-        dir="rtl"
-        bind:value={cost} />
-    </label>
-    <label class="flex flex-col">
-      <span class="text-sm font-medium">תאריך תשלום</span>
-      <input
-        type="date"
-        class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
-        dir="rtl"
-        bind:value={date} />
-    </label>
-    <label class="flex flex-col">
-      <span class="text-sm font-medium">הערות</span>
-      <textarea
-        class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
-        dir="rtl"
-        bind:value={notes}></textarea>
-    </label>
-  </div>
-  <button
-    class="tracking-wide px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded self-end"
-    on:click={() => addRow({ item, cost, notes, date })}>שמירת פריט</button>
+  <form on:submit={onSubmit}>
+    <div class="grid grid-flow-row gap-y-3 mb-3">
+      <label class="flex flex-col">
+        <span class="text-sm font-medium">שם הפריט</span>
+        <input
+          type="text"
+          class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
+          dir="rtl"
+          bind:value={item}
+          bind:this={itemNameInput}
+          required={true} />
+      </label>
+      <label class="flex flex-col">
+        <span class="text-sm font-medium">עלות הפריט</span>
+        <input
+          type="number"
+          class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
+          dir="rtl"
+          bind:value={cost}
+          required={true} />
+      </label>
+      <label class="flex flex-col">
+        <span class="text-sm font-medium">תאריך תשלום</span>
+        <input
+          type="date"
+          class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
+          dir="rtl"
+          bind:value={date}
+          required={true} />
+      </label>
+      <label class="flex flex-col">
+        <span class="text-sm font-medium">הערות</span>
+        <textarea
+          class="px-3 py-2 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 rounded text-gray-900"
+          dir="rtl"
+          bind:value={notes}></textarea>
+      </label>
+    </div>
+    <button
+      class="tracking-wide px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded self-end"
+      type="submit">שמירת פריט</button>
+  </form>
 </div>
