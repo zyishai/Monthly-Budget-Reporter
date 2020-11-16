dfcd89315080675bf474dceab5febe401b113f4c
Yishai Zehavi <zehaviyishai@gmail.com>
2020-11-16 21:17:02 +0200

    adjusted categories page to mobile

--- src/components/CategoriesTable.svelte
+++ src/components/CategoriesTable.svelte
@@ -1,12 +1,17 @@
 <script>
   import { getContext } from 'svelte';
+  import { slide } from 'svelte/transition';
+  import { writable } from 'svelte/store';
   import { categories, deleteCategory, updateCategoryInfo } from '../stores/categories';
-import ConfirmCategoryDelete from './modals/ConfirmCategoryDelete.svelte';
+  import ConfirmCategoryDelete from './modals/ConfirmCategoryDelete.svelte';
   import EditCategory from './modals/EditCategory.svelte';
+  import Number from './Number.svelte';
+  import LeftCarret from './svg/LeftCarret.svelte';
   import Pen from './svg/Pen.svelte';
   import Trash from './svg/Trash.svelte';
 
   const { open, close } = getContext('simple-modal');
+  const activeItem = writable(undefined);
 
   const updateCategory = (category) => () => {
     open(EditCategory, {
@@ -39,7 +44,7 @@ import ConfirmCategoryDelete from './modals/ConfirmCategoryDelete.svelte';
   }
 </script>
 
-<table class="{$$props.class} min-w-full leading-normal overflow-hidden">
+<table class="{$$props.class} hidden sm:contents min-w-full leading-normal overflow-hidden">
   <thead class="bg-gray-200 border-b-2 border-gray-300">
       <tr class="flex">
           <th
@@ -89,3 +94,44 @@ import ConfirmCategoryDelete from './modals/ConfirmCategoryDelete.svelte';
       {/each}
   </tbody>
 </table>
+
+<div class="{$$props.class} sm:hidden min-w-full leading-normal overflow-hidden">
+  <div class="sm:hidden min-w-full grid grid-flow-row divide-y divide-gray-200 overflow-y-auto">
+    {#each $categories as category}
+      <div class="p-2">
+        <div class="flex justify-between items-center cursor-pointer" on:click={() => activeItem.set($activeItem === category.id ? undefined : category.id)}>
+          <div class="flex items-center gap-x-1">
+            <LeftCarret class="h-2 w-2 {category.id === $activeItem && 'transform -rotate-90'}" />
+            <strong class="text-base font-medium tracking-wide">{category.name}</strong>
+          </div>
+          <span class="text-base">
+            <Number currency='₪'>
+              {category.maxExpense}
+            </Number>
+          </span>
+        </div>
+        {#if category.id === $activeItem}
+          <div 
+            transition:slide={{
+              duration: 200
+            }}>
+            <div class="flex justify-end items-center gap-x-2">
+              <button 
+              class="flex items-center gap-x-1 px-2 py-1 bg-red-200 hover:bg-red-300 rounded"
+              on:click={confirmDelete(category.id)}>
+              <Trash class="w-3 h-3" />
+              <span class="text-xs">מחיקה</span>
+            </button>
+            <button 
+              class="flex items-center gap-x-1 px-2 py-1 bg-gray-200 hover:bg-gray-300 rounded"
+              on:click={updateCategory(category)}>
+              <Pen class="w-3 h-3" />
+              <span class="text-xs">עריכה</span>
+            </button>
+            </div>
+          </div>
+        {/if}
+      </div>
+    {/each}
+  </div>
+</div>
--- src/routes/categories.svelte
+++ src/routes/categories.svelte
@@ -6,23 +6,24 @@
 
   const { page } = stores();
   let returnUrl = '/view';
-  $: returnUrl = $page.query.returnUrl;
+  $: returnUrl = $page.query.returnUrl ?? returnUrl;
 </script>
 
 <svelte:head>
 	<title>ניהול תקציב חודשי | קטגוריות</title>
 </svelte:head>
 
-<div class="flex-1 w-2/3 py-4 px-2 mx-auto flex flex-col overflow-hidden">
+<div class="flex-1 min-w-full sm:min-w-0 sm:w-2/3 py-2 sm:py-4 px-3 sm:px-2 mx-auto flex flex-col overflow-hidden">
   <div class="flex justify-between items-center">
     <div class="flex items-center gap-x-2">
-      <a rel="prefetch" href="{returnUrl}">
+      <a rel="prefetch" href="{returnUrl}" class="hidden sm:inline-block">
         <RightArrow class="h-5 w-5 -mt-1" />
       </a>
-      <h1 class="text-2xl font-semibold tracking-wide mb-1">עריכת קטגוריות</h1>
+      <h1 class="text-xl sm:text-2xl font-semibold tracking-wide mb-1">עריכת קטגוריות</h1>
     </div>
-    <AddCategoryButton />
+    <AddCategoryButton class="hidden sm:inline-block" />
   </div>
 
   <CategoriesTable class="flex-1 flex flex-col border-b border-gray-200 mb-2 overflow-hidden" />
+  <AddCategoryButton class="sm:hidden inline-block items-stretch mb-3" />
 </div>
