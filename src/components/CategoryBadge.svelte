342f56f023c56278c39db6b4b3276432345f1e01
Yishai Zehavi <zehaviyishai@gmail.com>
2021-03-29 13:16:20 +0300

    created badge component for categories

---
+++ src/components/CategoryBadge.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let onClick = () => {};
+  export let meta = {};
+
+  function badgeClickHandler() {
+    onClick(meta);
+  }
+</script>
+
+<div 
+  class="{$$props.class} badge inline-block px-2 py-1 border-2 border-gray-300 hover:border-gray-500 hover:bg-gray-200 text-gray-400 hover:text-gray-600 rounded-full cursor-pointer"
+  on:click="{badgeClickHandler}">
+  <span class="text-xs tracking-wider">
+    <slot></slot>
+  </span>
+</div>
