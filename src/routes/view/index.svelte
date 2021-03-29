06cc8806f46ed11024b918ccf1c1c82f584ae342
Yishai Zehavi <zehaviyishai@gmail.com>
2021-03-29 13:15:55 +0300

    custom message for mobile users

--- src/routes/view/index.svelte
+++ src/routes/view/index.svelte
@@ -1,4 +1,4 @@
-<div class="flex-1 flex flex-col justify-center items-center">
+<div class="hidden flex-1 sm:flex flex-col justify-center items-center">
   <h1 class="text-3xl font-bold tracking-wide">
     ברוך הבא!
   </h1>
@@ -7,5 +7,16 @@
   </h2>
   <p class="tracking-wide">
     ע״מ להתחיל בחר קטגוריה מהקטגוריות שבתפריט הימני או צור קטגוריה חדשה.
+  </p>
+</div>
+<div class="sm:hidden flex-1 text-center max-w-md mx-auto mt-4">
+  <h1 class="text-3xl font-bold tracking-wide">
+    ברוך הבא!
+  </h1>
+  <h2 class="text-xl font-medium tracking-wide">
+    כאן תוכל לצפות בהוצאות שלך ולנהל אותן.
+  </h2>
+  <p class="tracking-wide">
+    ע״מ להתחיל בחר קטגוריה משורת הקטגוריות או צור קטגוריה חדשה.
   </p>
 </div>
