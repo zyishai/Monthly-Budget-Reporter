4ad2fff93878551d7bead18de0a6052251d6aca8
Yishai Zehavi <zehaviyishai@gmail.com>
2021-05-02 17:30:32 +0300

    added updated category budget component

---
+++ src/components/UpdatedCategoryBudget.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { getMonthlyStats } from "../stores/stats";
+  import Number from "./Number.svelte";
+
+  export let category;
+  
+  const stats = getMonthlyStats();
+  $: categoryBudget = $stats.categories.find(({ id }) => id === category.id)?.diff || 0;
+</script>
+
+<p class="text-sm">
+  <span>
+    כמה נותר לי?
+  </span>
+  <strong class="tracking-wide">
+    <Number currency='₪'>{Math.max(categoryBudget, 0)}</Number>
+  </strong>
+</p>
--- src/routes/view/[type].svelte
+++ src/routes/view/[type].svelte
@@ -11,22 +11,24 @@
 
 <script>
   import AddExpenseButton from '../../components/AddExpenseButton.svelte';
-  import ExpenseViewStats from '../../components/ExpenseViewStats.svelte';
+  // import ExpenseViewStats from '../../components/ExpenseViewStats.svelte';
   import ExpensesTable from '../../components/ExpensesTable.svelte';
   import ExpensesList from '../../components/ExpensesList.svelte';
+import UpdatedCategoryBudget from '../../components/UpdatedCategoryBudget.svelte';
 
   export let category;
   export let categoryId;
 </script>
 
-<div class="flex-1 h-full flex flex-col items-stretch px-2 sm:p-4 overflow-hidden">
+<div class="flex flex-col items-stretch flex-1 h-full px-2 overflow-hidden sm:p-4">
   {#if $category}
-    <div class="flex items-center sm:justify-between mb-2">
+    <div class="flex items-center mb-2 sm:justify-between">
       <div>
         <h1 class="text-xl tracking-wide">
           <span>קטגוריה: </span>
           <strong>{$category.name}</strong>
         </h1>
+        <UpdatedCategoryBudget category={$category} />
         <!-- <p class="text-sm">
           <span>רף הוצאות מקסימלי: </span>
           <strong class="tracking-wide">{$category.maxExpense} ₪</strong>
@@ -37,17 +39,17 @@
       </div>
     </div>
     <div class="hidden sm:flex-1 sm:flex sm:flex-col sm:overflow-hidden">
-      <ExpensesTable {categoryId} class="flex-1 border-b border-gray-200 mb-2 overflow-hidden" />
+      <ExpensesTable {categoryId} class="flex-1 mb-2 overflow-hidden border-b border-gray-200" />
     </div>
-    <div class="flex-1 flex flex-col overflow-hidden sm:hidden">
-      <ExpensesList {categoryId} class="flex-1 border-b border-gray-200 mb-2 overflow-hidden" />
+    <div class="flex flex-col flex-1 overflow-hidden sm:hidden">
+      <ExpensesList {categoryId} class="flex-1 mb-2 overflow-hidden border-b border-gray-200" />
     </div>
-    <div class="sm:hidden flex flex-col items-stretch mb-3">
+    <div class="flex flex-col items-stretch mb-3 sm:hidden">
       <AddExpenseButton {categoryId} />
     </div>
-    <div class="mb-3">
+    <!-- <div class="mb-3">
       <ExpenseViewStats category={$category} />
-    </div>
+    </div> -->
   {:else}
     <h2 class="text-2xl">טוען נתונים...</h2>
   {/if}
